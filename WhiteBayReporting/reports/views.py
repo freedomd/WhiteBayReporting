6bd3c2db4d4ad4e1aa2e50cc2fe8ba06b5e16e73
rongdi <rh1424@nyu.edu>
2013-02-06 16:59:22 -0500

    bug fixed

--- WhiteBayReporting/reports/views.py
+++ WhiteBayReporting/reports/views.py
@@ -11,16 +11,26 @@ def reportView(request):
     delta = datetime.timedelta(days=1)
     yesterday = today - delta
     
-    print today
-    print yesterday
+#    try:
+#        report_list = Report.objects.filter(Q(reportDate__year=today.year) & Q(reportDate__month=today.month) & Q(reportDate__day=today.day))
+#        if report_list == None:
+#            report_list = Report.objects.filter(Q(reportDate__year=yesterday.year) & Q(reportDate__month=yesterday.month) & Q(reportDate__day=yesterday.day))
+#            report_date = yesterday
+#        else:
+#            report_date = today
+#        total = report_list.get(symbol="Total")
+#        report_list = report_list.exclude(symbol="Total")
+#    except:
+#        return HttpResponseRedirect("/")
     
+    # get latest reports
     try:
-        report_list = Report.objects.filter(Q(reportDate__year=today.year) & Q(reportDate__month=today.month) & Q(reportDate__day=today.day))
-        if report_list == None:
-            report_list = Report.objects.filter(Q(reportDate__year=yesterday.year) & Q(reportDate__month=yesterday.month) & Q(reportDate__day=yesterday.day))
-            report_date = yesterday
+        report_list = Report.objects.all().order_by('-reportDate')
+        if report_list != None:
+            report_date = report_list[0].reportDate
+            report_list = Report.objects.filter(Q(reportDate__year=report_date.year) & Q(reportDate__month=report_date.month) & Q(reportDate__day=report_date.day))
         else:
-            report_date = today
+            return HttpResponseRedirect("/")
         total = report_list.get(symbol="Total")
         report_list = report_list.exclude(symbol="Total")
     except:
