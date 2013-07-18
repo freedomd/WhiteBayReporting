649b1556e6bc1ad01096afd7b3a103e17aa8ccd3
gunitzz <edison890823@msn.com>
2013-07-18 15:58:58 -0400

    fix

--- WhiteBayReporting/trades/models.py
+++ WhiteBayReporting/trades/models.py
@@ -20,10 +20,6 @@ class Trade(models.Model):
     executionId = models.CharField( max_length=100, default="0" )
     baseMoney = models.FloatField( default=0.00 )
     description = models.CharField( max_length=100, default="NORMAL TRADE")
-#    clearanceFees = models.FloatField( default=0.00 )
-#    brokerCommission = models.FloatField( default=0.00 )
-#    commission = models.FloatField( default=0.00 )
-#    secFees = models.FloatField( default=0.00 )
     ecnFees = models.FloatField( default=0.00 )
     
     def save(self, *args, **kwargs): 
@@ -52,7 +48,6 @@ class RollTrade(models.Model):
     tradeDate = models.DateField( auto_now_add=False )
     baseMoney = models.FloatField( default=0.00 )
     description = models.CharField( max_length=100, default="NORMAL TRADE")
-#    clearanceFees = models.FloatField( default=0.00 )
     
     # after rolling, this ecnFees is the sum of ecnFees of the rolled trades
     ecnFees = models.FloatField( default=0.00 )
